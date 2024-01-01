3866f322b3404dd449b24e1ea850a706f1fb1102
Kaczupinko <kaczupinko@gmail.com>
2024-01-01 21:32:43 +0000

    Use static

--- src/components/Div.js
+++ src/components/Div.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { TextContext } from './../context';
 
 export default class Div extends React.Component {
+    static contextType = TextContext;
+    
     render() {
         return (
             <div>{ this.context }</div>
         )
     }
 }
-
-Div.contextType = TextContext;
