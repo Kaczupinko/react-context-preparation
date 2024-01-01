f86073855a44d081b6e55828f7306ab72e456915
Kaczupinko <kaczupinko@gmail.com>
2024-01-01 23:15:16 +0000

    Practice with useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,20 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
     const [counter, setCounter] = useState(0);
+    const [entered, setEntered] = useState(0);
     useEffect(() => {
         console.log("counter: ", counter);
-    }, []);
+    }, [counter]);
 
+    useEffect(() => {
+        document.title = `${counter} / ${entered}`;
+        }, [counter, entered]);
     return (
         <div>
             <button
+                onMouseEnter={ e => setEntered(entered + 1)}
                 onClick={ e => setCounter(counter + 1) }
-            >{ counter }</button>
+            >{ counter } / { entered}</button>
         </div>
     )
 }
