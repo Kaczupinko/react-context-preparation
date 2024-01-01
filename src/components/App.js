4650930323cf3b435f766aef1753eb905b05a499
Kaczupinko <kaczupinko@gmail.com>
2024-01-01 23:16:47 +0000

    Try to use setInterval with useEffectr

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,15 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
     const [counter, setCounter] = useState(0);
-    const [entered, setEntered] = useState(0);
-    useEffect(() => {
-        console.log("counter: ", counter);
-    }, [counter]);
 
     useEffect(() => {
-        document.title = `${counter} / ${entered}`;
-        }, [counter, entered]);
+        setInterval(() => {
+            setCounter(value => value + 1);
+        }, 1000);
+    }, []);
+
     return (
-        <div>
-            <button
-                onMouseEnter={ e => setEntered(entered + 1)}
-                onClick={ e => setCounter(counter + 1) }
-            >{ counter } / { entered}</button>
-        </div>
+        <div>{ counter }</div>
     )
 }
 
