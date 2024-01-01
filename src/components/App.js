76908cfd97c853c1116a48bbc09afe683504a812
Kaczupinko <kaczupinko@gmail.com>
2024-01-01 23:09:17 +0000

    Add second parameter to useeffect

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,9 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
     const [counter, setCounter] = useState(0);
-
     useEffect(() => {
         console.log("counter: ", counter);
-    });
+    }, []);
 
     return (
         <div>
